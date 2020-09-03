3b8395122fa0433053e69dd7cf05f49b5e56b8f9
KaranTrivedi <kytrivedi94@hotmail.com>
2020-09-03 13:30:33 -0400

    Minor changes

--- maker.py
+++ maker.py
@@ -1,5 +1,10 @@
 #!./venv/bin/python
 
+"""[Create python project with best practices.]
+
+    This project allows user to get started with a new python project with some default features in place such as logging, service file, etc. in place.
+"""
+
 import configparser
 import json
 import logging
@@ -8,20 +13,20 @@ import argparse
 
 #Define config and logger.
 CONFIG = configparser.ConfigParser()
-CONFIG.read('conf/config.ini')
-SECTION = 'maker'
+CONFIG.read("conf/config.ini")
+SECTION = "maker"
 
-logging.basicConfig(filename=CONFIG[SECTION]['log'],\
-                    level=CONFIG[SECTION]['level'],\
+logging.basicConfig(filename=CONFIG[SECTION]["log"],\
+                    level=CONFIG[SECTION]["level"],\
                     format='%(asctime)s::%(name)s::%(funcName)s::%(levelname)s::%(message)s',\
                     datefmt='%Y-%m-%d %H:%M:%S')
 
 logger = logging.getLogger(SECTION)
 
 def show_sections():
-    '''
+    """
     Output all options for given section
-    '''
+    """
 
     conf_str = "\n\n"
     for sect in CONFIG.sections():
@@ -31,27 +36,28 @@ def show_sections():
     logger.info(conf_str)
 
 class Sample:
-    '''
+    """
     Create class
-    '''
+    """
 
     def __init__(self, var):
         self.var = var
 
     def __str__(self):
-        '''
+        """
         stringify
-        '''
+        """
         return json.dumps(vars(self), indent=2)
 
 
 def main():
-    '''
+    """
     Main function.
-    '''
+    """
+
     logger.info("####################STARTING####################")
 
-    if CONFIG[SECTION]['level'] == "DEBUG":
+    if CONFIG[SECTION]["level"] == "DEBUG":
         show_sections()
 
 if __name__ == "__main__":
